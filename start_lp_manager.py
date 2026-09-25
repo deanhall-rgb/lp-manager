3a077c53933723c5c609d64afc3bf7a7e586ed10
deanhall-rgb <dean.hutiktok@gmail.com>
2026-09-25 17:07:41 +0100

    V0.8.11 launcher: update version identity

--- start_lp_manager.py
+++ start_lp_manager.py
@@ -9,7 +9,7 @@ import uvicorn
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser(description="Start LP Manager v0.8.10 Stabilisation")
+    parser = argparse.ArgumentParser(description="Start LP Manager v0.8.11 Stabilisation Fixes")
     parser.add_argument("--host", default="127.0.0.1")
     parser.add_argument("--port", type=int, default=8765)
     parser.add_argument("--no-browser", action="store_true")
